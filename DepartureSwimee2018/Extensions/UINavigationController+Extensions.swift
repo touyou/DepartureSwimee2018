b622b63b4742f855119537f448a73c05d195e9dd
touyou <fujiyou1121@gmail.com>
2018-03-31 11:41:11 +0900

    add protocol and extensions

---
+++ DepartureSwimee2018/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  UINavigationController+Extensions.swift
+//  GitMe
+//
+//  Created by 藤井陽介 on 2018/02/09.
+//  Copyright © 2018 touyou. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Project Layout
+
+extension UINavigationController {
+
+    func setupBarColor() {
+
+        self.navigationBar.barTintColor = UIColor.Swimee.barOrange
+        self.navigationBar.tintColor = UIColor.white
+        self.navigationBar.backgroundColor = UIColor.Swimee.barOrange
+        if #available(iOS 11.0, *) {
+            self.navigationBar.prefersLargeTitles = true
+        }
+    }
+}
